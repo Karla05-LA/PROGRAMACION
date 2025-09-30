ae39d23ba2d7861ff40f3610606cfeacf9079277
Karla05-LA <karla.herrerag05@gmail.com>
2025-09-30 16:00:57 -0600

    Add files via upload

---
+++ 100 EJERCICIOS/58 AL 69/61.py
@@ -0,0 +1,11 @@
+def calcular_perimetro(base, altura):
+    return 2 * (base + altura)
+
+#Ejemplo
+base = 17
+altura = 7
+
+resultado = calcular_perimetro(base,altura)
+print("El resultado del perimetro es : ", resultado)
+
+
