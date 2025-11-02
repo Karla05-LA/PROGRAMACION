d44cf738f5b58d55cfe977c5637137cb02ce016f
Karla05-LA <karla.herrerag05@gmail.com>
2025-11-02 13:42:37 -0600

    Add mathematical calculations using math module

---
+++ 100 EJERCICIOS/70 al 84/70.py
@@ -0,0 +1,11 @@
+
+import math as mt
+
+a = mt.sin(mt.radians(45)) + mt.cos(mt.pi)
+print("a) Seno de 45 + coseno de pi =", a)
+
+b = mt.sqrt(mt.e ** 2)
+print("b) Raíz de euler elevado a la dos =", b)
+
+c = round(mt.pi * mt.sqrt(2), 3)
+print("c) Multiplicación entre pi y raíz de dos (3 decimales) =", c)
