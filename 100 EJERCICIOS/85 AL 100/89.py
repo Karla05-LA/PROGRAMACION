ab1ed64634b6988d30d88669d0c3f6c76db3b6db
Karla05-LA <karla.herrerag05@gmail.com>
2025-11-13 13:37:22 -0600

    Create 89.py

---
+++ 100 EJERCICIOS/85 AL 100/89.py
@@ -0,0 +1,12 @@
+
+import re
+
+contrasena = input("Ingrese una contraseña: ")
+
+if (len(contrasena) >= 8 and
+    re.search(r'[A-Z]', contrasena) and
+    re.search(r'[a-z]', contrasena) and
+    re.search(r'\d', contrasena)):
+    print("Contraseña válida")
+else:
+    print("Contraseña inválida")
