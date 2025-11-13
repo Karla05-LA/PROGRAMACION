e8ad69e62461bfaf81354aa0942ba1020c8e0f55
Karla05-LA <karla.herrerag05@gmail.com>
2025-11-13 13:38:22 -0600

    Update 89.py

--- 100 EJERCICIOS/85 AL 100/89.py
+++ 100 EJERCICIOS/85 AL 100/89.py
@@ -7,6 +7,6 @@ if (len(contrasena) >= 8 and
     re.search(r'[A-Z]', contrasena) and
     re.search(r'[a-z]', contrasena) and
     re.search(r'\d', contrasena)):
-    print("Contraseña válida")
+    print("Contraseña segura")
 else:
-    print("Contraseña inválida")
+    print("Contraseña insegura")
