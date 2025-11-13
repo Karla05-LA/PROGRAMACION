871c59451d788eeddf9176a7a76db7eef6350420
Karla05-LA <karla.herrerag05@gmail.com>
2025-11-13 13:47:17 -0600

    Fix print statements for response messages

--- 100 EJERCICIOS/85 AL 100/97.py
+++ 100 EJERCICIOS/85 AL 100/97.py
@@ -10,6 +10,6 @@ if response.status_code == 200:
     data = response.json()
     with open("respuesta.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-    print("✅ Respuesta guardada en 'respuesta.json'")
+    print("Respuesta guardada en 'respuesta.json'")
 else:
-    print("❌ Error al hacer la petición:", response.status_code)
+    print("Error al hacer la petición:", response.status_code)
