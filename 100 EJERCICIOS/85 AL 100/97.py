ae6f138e135bb4b317c28f7435d01a5ec9bd0774
Karla05-LA <karla.herrerag05@gmail.com>
2025-11-13 13:47:40 -0600

    Update 97.py

--- 100 EJERCICIOS/85 AL 100/97.py
+++ 100 EJERCICIOS/85 AL 100/97.py
@@ -8,6 +8,7 @@ response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
+    
     with open("respuesta.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
     print("Respuesta guardada en 'respuesta.json'")
